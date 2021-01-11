bffa72275c7a87a5e7d08a5e40502af5d30d64a2
iwantutobemyside <c.thumasorn@gmail.com>
2021-01-12 00:19:04 +0700

    1st ver

--- game.py
+++ game.py
@@ -6,22 +6,27 @@ while player == False:
     player = input("Rock, Paper, Scissors? : ")
     if player == computer:
         print("It's Tie!!","becoz", "computer = ",computer)
+    
     elif player == "Rock":
         if computer == "Paper":
             print("You lose!", "becoz", "computer = ",computer)
         else:
             print("You win!", "becoz", "computer = ",computer)
+    
     elif player == "Paper":
         if computer == "Rock":
             print("You win!", "becoz", "computer = ",computer)
         else:
             print("You lose!", "becoz", "computer = ",computer)
+    
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose!", "becoz", "computer = ",computer)
         else:
             print("You win!", "becoz", "computer = ",computer)
     else:
-        print("Not a Valid play bro, Pls check ur spelling")
+        print("Not a valid play bro, Pls check ur spelling")
+
 player = False
 computer = bot[randint(0,2)]
+print("Finished")
